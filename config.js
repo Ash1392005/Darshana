d8bd3b50de461987007b89d744d5f58663997755
Ajay-o-s <akhubsmaster@gmail.com>
2022-03-29 18:30:25 +0530

    main update to start Darshana

--- config.js
+++ config.js
@@ -10,14 +10,17 @@ DEBUG = process.env.DEBUG === undefined ? false : convertToBool(process.env.DEBU
 module.exports = {
     VERSION: 'v1.0.9',
     AMMU_CODE: process.env.AMMU_CODE === undefined ? '' : process.env.AMMU_CODE,
-    BOT_PRESENCE: process.env.BOT_PRESENCE === undefined ? 'online' : process.env.BOT_PRESENCE,
-    NO_LOG: process.env.NO_LOG === undefined ? 'false' : process.env.NO_LOG,
-    WORKTYPE: process.env.WORK_TYPE === undefined ? 'private' : process.env.WORK_TYPE,
     HANDLERS: process.env.HANDLERS === undefined ? '[.!;]' : process.env.HANDLERS,
+    WORKTYPE: process.env.WORK_TYPE === undefined ? 'private' : process.env.WORK_TYPE,
+    BOT_PRESENCE: process.env.BOT_PRESENCE === undefined ? 'online' : process.env.BOT_PRESENCE,
+    SUDO: process.env.SUDO === undefined ? '919188346721,0' : process.env.SUDO,
+    NO_LOG: process.env.NO_LOG === undefined ? 'false' : process.env.NO_LOG,
     SEND_READ: process.env.SEND_READ === undefined ? false : convertToBool(process.env.SEND_READ),
+    NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
     AUTO_UPDATE: process.env.AUTO_UPDATE === undefined ? false : convertToBool(process.env.AUTO_UPDATE),
     MENTION: process.env.MENTION === undefined ? 'true' : process.env.MENTION,
     AK_MENTION: process.env.AK_MENTION === undefined ? 'true' : process.env.AK_MENTION,
+    PASSWORD: process.env.PASSWORD === undefined ? 'darsshana' : process.env.PASSWORD,
     STATUS: process.env.STATUS === undefined ? 'false' : process.env.STATUS,
     BRANCH: 'main',
     HEROKU: {
@@ -25,8 +28,6 @@ module.exports = {
         API_KEY: process.env.HEROKU_API_KEY === undefined ? '' : process.env.HEROKU_API_KEY,
         APP_NAME: process.env.HEROKU_APP_NAME === undefined ? '' : process.env.HEROKU_APP_NAME
     },
-    NO_ONLINE: process.env.NO_ONLINE === undefined ? true : convertToBool(process.env.NO_ONLINE),
     DATABASE: new Sequelize({ dialect: "sqlite", storage: './server/DB/Darshana.db', logging: DEBUG }),
-    SUDO: process.env.SUDO === undefined ? '919188346721,0' : process.env.SUDO,
     DEBUG: DEBUG 
 };
